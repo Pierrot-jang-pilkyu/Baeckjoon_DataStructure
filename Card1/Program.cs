18896e13fff8cfbae237ae922d66157d1e50d9f8
Pierrot-Jang-pilkyu <jangpilkyu1@gmail.com>
2023-03-10 21:32:11 +0900

    03/10-1

---
+++ Card1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Card1
+{
+	static string n = null;
+	static List<int> clist = new List<int>();
+	static List<int> rlist = new List<int>();
+
+	static void Input()
+	{
+		n = Console.ReadLine();
+		
+		for (int i = 1; i <= int.Parse(n); i++)
+			clist.Add(i);
+	}
+
+	static void Solution()
+	{
+		while (clist.Count != 1)
+		{
+			rlist.Add(clist[0]);
+			clist.RemoveAt(0);
+			if (clist.Count == 1)
+				break ;
+			clist.Add(clist[0]);
+			clist.RemoveAt(0);
+		}
+		rlist.Add(clist[0]);
+	}
+
+	static void Output()
+	{
+        string res = "";
+        
+		for (int i = 0; i < int.Parse(n); i++)
+		{
+			res += rlist[i].ToString();
+			if (i != int.Parse(n) - 1)
+				res += " ";
+		}
+        Console.Write(res);
+	}
+
+	public static void Main()
+	{
+		Input();
+		Solution();
+		Output();
+	}
+}
